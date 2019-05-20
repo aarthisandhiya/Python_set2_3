ab11906b4007a622c174b0e287a61a042e7d8650
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-20 11:29:25 +0530

    Update 8hunter.py

--- 8hunter.py
+++ 8hunter.py
@@ -1,8 +1,9 @@
 a=int(input())
 s=' '
 b=[int(a) for a in input().split()]
-for i in range(0,len(b)):
-    for j in range(0,len(b)):
-        for k in range(0,len(b)):
+for i in range(0,a-2):
+    for j in range(i+1,a-1):
+        for k in range(j+1,a):
             if b[i]+b[j]==b[k] and i<j<k:
-                print(b[i],' ',b[j],' ',b[k])
+                print(b[i],b[j],b[k])
+                break
