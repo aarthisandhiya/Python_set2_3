605efb907e6a794aa543a06bd4c82efd2bdaf406
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-30 12:57:19 +0530

    Create 122player.py

---
+++ 122player.py
@@ -0,0 +1,26 @@
+a=input()
+b=a.split("-")
+if b[1]=="01":
+    print("January")
+elif b[1]=="02":
+    print("February")
+elif b[1]=="03":
+    print("March")
+elif b[1]=="04":
+    print("April")
+elif b[1]=="05":
+    print("May")
+elif b[1]=="06":
+    print("June")
+elif b[1]=="07":
+    print("July")
+elif b[1]=="08":
+    print("August")
+elif b[1]=="09":
+    print("September")
+elif b[1]=="10":
+    print("October")
+elif b[1]=="11":
+    print("November")
+elif b[1]=="12":
+    print("December")
