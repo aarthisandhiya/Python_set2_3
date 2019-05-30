3342962ebcd2d5a3912b47971d4a44b56927ed35
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-30 14:33:51 +0530

    Rename 68hunter.py to 68hunter1.py

---
+++ 68hunter1.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=[int(a) for a in input().split()]
+s=min(b)
+k=max(b)
+for i in range(0,len(b)):
+    if b[i]==s:
+        print(i+1,end=" ")
+    elif b[i]==k:
+        print(i+1,end=" ")
