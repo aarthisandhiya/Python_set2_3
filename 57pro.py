cadbddcc8d8acd0d5e85767d0fbac940fbb2569a
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-31 13:58:53 +0530

    Create 57pro.py

---
+++ 57pro.py
@@ -0,0 +1,11 @@
+a,b=map(str,input().split())
+c=0
+for i in range(0,len(a)):
+    for j in range(i,len(b)):
+        if a[i]==b[j]:
+            c=c+1
+            print(a[i],"==",b[j])
+if c>=2:
+    print("yes")
+else:
+    print("no")
