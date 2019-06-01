1c176fb48e8e0a5be6e853333dfa2d66c4a9b350
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-06-01 18:49:30 +0530

    Update 111hunter.py

--- 111hunter.py
+++ 111hunter.py
@@ -6,6 +6,6 @@ for i in range(a):
     d=[int(a) for a in input().split()]
     s.append(d)
 #print(s)
-dumm=sum(s[i][i] for i in range(a))
+dumm=sum(s[a-i-1][a-i-1] for i in range(a))
 print(dumm)
     
