fd7cb29977e66812af64e033e611e4fbe0ec5131
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-06-07 17:12:41 +0530

    Update 63pro.py

--- 63pro.py
+++ 63pro.py
@@ -1,17 +1,9 @@
 a=input()
 lis=[]
-if len(a)==1:
-    print("1")
-elif len(a)==2:
-    s=a[0]
-    h=a[1]
-    if s==h:
-        print("1")
+for i in a:
+    if i not in lis:
+        lis.append(i)
+        print(i)
     else:
-        print("2")
-else:
-    for i in range(0,len(a)-1):
-        for j in range(i+1,len(a)):
-            if a[i]!=a[j] and a[i] not in lis:
-                lis.append(a[i])
-    print(len(lis)+1)
+        break
+print(len(lis))
