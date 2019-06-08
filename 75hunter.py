a4d46ad9cece4bdd3e382843e32456f36b4091a3
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-06-08 11:02:03 +0530

    Create 75hunter.py

---
+++ 75hunter.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=[int(a) for a in input().split()]
+for i in range(0,len(b)-1):
+    j=i+1 
+    if b[j]<b[i]:
+        print(b[j],end=" ")
+    else:
+        print("-1",end=" ")
+else:
+    if b[-1]>b[0]:
+        print(b[-1])
+    else:
+        print("-1")
+        
