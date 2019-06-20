804702a59db5b9f0457f1d89f5c9cace1524c18d
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-06-20 11:33:43 +0530

    Update 19hunter.py

--- 19hunter.py
+++ 19hunter.py
@@ -2,21 +2,13 @@ a=int(input())
 s=[]
 kit=0
 for i in range(2,a):
-    for j in range(2,i):
-        if i%j!=0:
-            kit=0
-    else:
-        kit=kit+1
-if kit==0:
+    if a%i==0:
+        for j in range(2,i):
+            if i%j==0:
+                break 
+        else:
+            s.append(i)
+if len(s)==0:
     print(a)
-elif kit>0:
-    for i in range(2,a):
-        if a%i==0:
-            for j in range(2,i):
-                if i%j==0:
-                    break 
-            else:
-                s.append(i)
+else:
     print(*s)
-
-
