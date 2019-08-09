7230e858bbe77e81cb342a18a9a2b84b4265af22
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-08-09 22:58:57 +0530

    Update 127hunt.py

--- 127hunt.py
+++ 127hunt.py
@@ -1,7 +1,8 @@
 a,b=map(str,input().split())
 lis=[]
-for i in b:
-    if i in a and i not in lis:
-        lis.append(i)
-if len(lis)==len(b):
-    print("".join(list(lis)))
+for i in range(len(b)):
+    for j in range(len(a)):
+        if b[i]==a[j] and a[j] not in lis:
+            lis.append(b[i])
+#print(lis)
+print("".join(list(lis)))
